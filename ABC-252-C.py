fdb3e28951ffddd1962f1910db568e2a686ac7af
Sadil <zokutaix@gmail.com>
2022-05-25 21:15:39 +0900

    ABC-２５２-C 何をすればいいかがよく理解できていないため、もう一度日を改めて考えることとする

--- ABC-252-C.py
+++ ABC-252-C.py
@@ -1,8 +1,16 @@
 N=int(input())
-S=input() for i in range(N)
-for i in range(10):
-    for j in range(N):
-        a=str(i)
-        b=s[j].index(a)  #index(a)でリスト内の特定の文字を引き抜ける
+S = []
+for i in range(N):
+  s = input()
+  S.append(s)
 
-print(min)　#わからん
+b=[0]*10
+for i in range(10):
+    c=[0]*10
+    for j in range(N):
+        a=int(S[j][i]) #標準入力した文字列を
+        c[a]+=1
+        b[a]=max(b[a],i+10*(c[a]-1))　#index(a)でリスト内の特定の文字を引き抜ける
+print(min(b))　#わからん
+
+#ゆっくり他の人の答えを見ながら写したが各文で何をしているかが理解できていない→A.Bを何度もやって解決できる？
